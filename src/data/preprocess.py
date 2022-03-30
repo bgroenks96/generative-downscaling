4ea8eaba7ba1d8174415104076f25b0609c13358
Jake White <jake@riskfuel.com>
2022-03-30 13:55:56 -0400

    corrected 64x64 processed data

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -17,34 +17,59 @@ log = logging.getLogger(__name__)
 
 
 EPSILON = .0001
-NA_LAT = (0, 95+EPSILON)  # epsilon to avoid non-inclusive right
-NA_LON = (-140, -50+EPSILON)  # epsilon to avoid non-inclusive right
+NA_LAT_FINE = (-5, 85+EPSILON)  # epsilon to avoid non-inclusive right
+NA_LON_FINE = (-137.5+180, -47.5+180+EPSILON)
+
+NA_LAT_COARSE = (-10, 88+EPSILON)  # Coarse should be wider for full coverage
+NA_LON_COARSE = (-145+180, -45+180+EPSILON)
+
 KELVIN_TO_CELSIUS = -273.15
 
 
-def north_america_only(weatherbench: xr.Dataset) -> xr.Dataset:
-    """ Filters the passed WEATHERBENCH dataset to North American coordinates only """
+def north_america_only(weatherbench: xr.Dataset, res: str) -> xr.Dataset:
+    """ Filters the passed WEATHERBENCH dataset to North American coordinates only
 
-    return weatherbench.sel(lat=slice(*NA_LAT), lon=slice(*NA_LON))
+    Arguments:
+      * weatherbench: a dataset derived from WeatherBench (but with centered longitude)
+      * res: resolution of the data. Either "1406" (1.4 deg) or "5625" (5.6 deg)
+    """
+    if res == "1406":
+        return weatherbench.sel(lat=slice(*NA_LAT_FINE), lon=slice(*NA_LON_FINE))
+    elif res == "5625":
+        return weatherbench.sel(lat=slice(*NA_LAT_COARSE), lon=slice(*NA_LON_COARSE))
+    else:
+        raise ValueError(f"Unrecognized resolution '{res}'")
 
 
 def filter_and_concatenate(
     fnames: List[os.PathLike],
-    metric: str
+    metric: str,
+    res: str
 ) -> xr.Dataset:
+    """ Creates a dataset from a set of Weatherbench files.
+
+    Filters to greater North America only (Artics through Central America),
+    Aggregates to daily max temperature or daily total precipitation.
+
+    Arguments:
+      * fnames: a list of WeatherBench file paths in netcdf4 format (.nc)
+      * metric: either "temp" (for temperature) or "precip" (for pressure)
+      * res: resolution of the data. Either "1406" (1.4 deg) or "5625" (5.6 deg)
+    """
+
     subsets = []
     for fname in tqdm(fnames):
         subset =  xr.open_dataset(fname)
 
         subset['lon'] = subset['lon'] - 180  # make longitude coordinates symmetric
-        subset_na = north_america_only(subset)
+        subset_na = north_america_only(subset, res=res)
 
-        if metric == 'temp':
+        if metric == "temp":
             subset_na_daily = subset_na.groupby('time.date').max() + KELVIN_TO_CELSIUS
-        else:
+        elif metric == "precip":
             subset_na_daily = subset_na.groupby('time.date').sum()
-
-        # TODO(jwhite) does any de-meaning need to occur here?
+        else:
+            raise ValueError(f"Unrecognized metric '{metric}'")
 
         subsets.append(subset_na_daily)
 
@@ -66,7 +91,7 @@ def process_raw_weatherbench(data_dir: os.PathLike) -> None:
             file_names = [os.path.join(raw_dir, fname) for fname in os.listdir(raw_dir)]
 
             log.info(f"processing {metric}/{res}...")
-            result = filter_and_concatenate(file_names, metric=metric)
+            result = filter_and_concatenate(file_names, metric=metric, res=res)
 
             dest_dir = os.path.join(data_dir,"processed",metric,res)
             if os.path.exists(dest_dir):
@@ -74,7 +99,6 @@ def process_raw_weatherbench(data_dir: os.PathLike) -> None:
                 shutil.rmtree(dest_dir)
             os.makedirs(dest_dir)
 
-            # import pdb; pdb.set_trace()
             dest_path =  os.path.join(dest_dir, f"{metric}_{res}_processed.zarr")
             result.to_zarr(dest_path)
 
